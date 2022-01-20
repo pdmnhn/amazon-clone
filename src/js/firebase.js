4558bc301ffe30176de66ceef793b6d418f32242
Padmanabhan <42073602+pdmnhn@users.noreply.github.com>
2022-01-20 22:50:16 +0530

    some minor fixes

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -9,12 +9,12 @@ import {
 // import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "***REMOVED***",
-  authDomain: ***REMOVED***,
-  projectId: "***REMOVED***",
-  storageBucket: ***REMOVED***,
-  messagingSenderId: "***REMOVED***",
-  appId: ***REMOVED***,
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
 const app = initializeApp(firebaseConfig);
